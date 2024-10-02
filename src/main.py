eff47f5ca1ab267e6b9ae11dfe60b79e0c599966
jose <jg.ramirez100@gmail.com>
2024-10-02 00:43:13 -0500

    feat: generate pages recursive

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 
 from markdown_blocks import extarct_title, markdown_to_html_node
 
@@ -11,7 +12,7 @@ def main():
         shutil.rmtree(public_path)
     os.mkdir(public_path)
     copy_files(static_path, public_path)
-    generate_page('./content/index.md', './template.html', './public/index.html')
+    generate_pages_recursive('./content/', './template.html', './public/')
 
 
 def copy_files(current_path, destination):
@@ -40,12 +41,23 @@ def generate_page(from_path, template_path, dest_path):
     formatted_template = template.replace('{{ Title }}', title).replace('{{ Content }}', html)
 
     dest_dir = os.path.dirname(dest_path)
-    os.makedirs(dest_dir, exist_ok=True)
+    if dest_dir != '':
+        os.makedirs(dest_dir, exist_ok=True)
 
     with open(dest_path, 'w', encoding='utf-8') as f:
         f.write(formatted_template)
 
 
-# def generate_pages_recursive(from_path, template_path, dest_path):
+def generate_pages_recursive(from_dir_path, template_path, dest_dir_path):
+    for f in os.listdir(from_dir_path):
+        origin_path = os.path.join(from_dir_path, f)
+        new_dest_path = os.path.join(dest_dir_path, f)
+
+        if os.path.isfile(origin_path):
+            destination_path = Path(new_dest_path).with_suffix('.html')
+            generate_page(origin_path, template_path, destination_path)
+        else:
+            generate_pages_recursive(origin_path, template_path, new_dest_path)
+
 
 main()
